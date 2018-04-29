0386fd1501ee751dd531cba934bba31aa1e3fc2f
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-30 01:08:36 +0300

    searcher implemented to router

--- app/router.js
+++ app/router.js
@@ -11,8 +11,13 @@ router.get('/', (req, res) => {
 router.get('/search/:query', (req, res) => {
     let query = decodeURIComponent(req.params.query);
     let offset = parseInt(req.query.offset);
-    // let results = searcher(query, offset);
-    res.json({query: query, offset: offset});
+    searcher(query, offset).then(results => {
+        res.json(results);
+    }).catch(err => {
+        console.error(err);
+        let reason = err.error.error.errors[0].reason;
+        res.json({ "error": reason });
+    })
 })
 
 module.exports = router;
