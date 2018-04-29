ca66da19f9423a7a3dfb0a98efc5e36872694863
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-29 23:25:19 +0300

    new dependencies added. getting offset from querystring is done.

--- app/router.js
+++ app/router.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const searcher = require('./searcher.js');
 var router = express.Router();
 
 // main page router
 router.get('/', (req, res) => {
     res.send("It works!");
 });
+
+// searcher router
+router.get('/search/:query', (req, res) => {
+    let query = decodeURIComponent(req.params.query);
+    let offset = parseInt(req.query.offset);
+    // let results = searcher(query, offset);
+    res.json({query: query, offset: offset});
+})
 
 module.exports = router;
